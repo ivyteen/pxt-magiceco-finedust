134dad7c15edcee28f34fa51293ba92dbcbcaacb
ivyteen <neorafale@gmail.com>
2019-08-09 16:44:26 +0900

    Initial update

---
+++ finedust.ts
@@ -0,0 +1,236 @@
+/**
+ * Custom blocks
+ */
+//% weight=90 color=#ff7a4b icon="\uf0ee" block="미세먼지"
+namespace Finedust {
+
+    // keep track of services
+    //let rainMonitorStarted = false;
+    //let windMonitorStarted = false;
+    let weatherMonitorStarted = false;
+    // Keep Track of weather monitoring variables
+    //let numRainDumps = 0
+    //let numWindTurns = 0
+    //let windMPH = 0
+
+    // BME280 Addresses
+    let BME280_I2C_ADDR = 0x76
+    let dig_T1 = getUInt16LE(0x88)
+    let dig_T2 = getInt16LE(0x8A)
+    let dig_T3 = getInt16LE(0x8C)
+    let dig_P1 = getUInt16LE(0x8E)
+    let dig_P2 = getInt16LE(0x90)
+    let dig_P3 = getInt16LE(0x92)
+    let dig_P4 = getInt16LE(0x94)
+    let dig_P5 = getInt16LE(0x96)
+    let dig_P6 = getInt16LE(0x98)
+    let dig_P7 = getInt16LE(0x9A)
+    let dig_P8 = getInt16LE(0x9C)
+    let dig_P9 = getInt16LE(0x9E)
+    let dig_H1 = getreg(0xA1)
+    let dig_H2 = getInt16LE(0xE1)
+    let dig_H3 = getreg(0xE3)
+    let a = getreg(0xE5)
+    let dig_H4 = (getreg(0xE4) << 4) + (a % 16)
+    let dig_H5 = (getreg(0xE6) << 4) + (a >> 4)
+    let dig_H6 = getInt8LE(0xE7)
+    let T = 0
+    let P = 0
+    let H = 0
+    setreg(0xF2, 0x04)
+    setreg(0xF4, 0x2F)
+    setreg(0xF5, 0x0C)
+    setreg(0xF4, 0x2F)
+
+    // Stores compensation values for Temperature (must be read from BME280 NVM)
+    let digT1Val = 0
+    let digT2Val = 0
+    let digT3Val = 0
+
+    // Stores compensation values for humidity (must be read from BME280 NVM)
+    let digH1Val = 0
+    let digH2Val = 0
+    let digH3Val = 0
+    let digH4Val = 0
+    let digH5Val = 0
+    let digH6Val = 0
+
+    // Buffer to hold pressure compensation values to pass to the C++ compensation function
+    let digPBuf: Buffer
+
+    // BME Compensation Parameter Addresses for Temperature
+    const digT1 = 0x88
+    const digT2 = 0x8A
+    const digT3 = 0x8C
+
+    // BME Compensation Parameter Addresses for Pressure
+    const digP1 = 0x8E
+    const digP2 = 0x90
+    const digP3 = 0x92
+    const digP4 = 0x94
+    const digP5 = 0x96
+    const digP6 = 0x98
+    const digP7 = 0x9A
+    const digP8 = 0x9C
+    const digP9 = 0x9E
+
+    // BME Compensation Parameter Addresses for Humidity
+    const digH1 = 0xA1
+    const digH2 = 0xE1
+    const digH3 = 0xE3
+    const e5Reg = 0xE5
+    const e4Reg = 0xE4
+    const e6Reg = 0xE6
+    const digH6 = 0xE7
+
+    let Reference_VOLTAGE = 3100
+
+
+
+    export enum DHT11Type {
+        //% block="temperature(℃)" enumval=0
+        DHT11_temperature_C,
+
+        //% block="temperature(℉)" enumval=1
+        DHT11_temperature_F,
+
+        //% block="humidity(0~100)" enumval=2
+        DHT11_humidity,
+    }
+
+
+    export enum Distance_Unit {
+        //% block="mm" enumval=0
+        Distance_Unit_mm,
+
+        //% block="cm" enumval=1
+        Distance_Unit_cm,
+
+        //% block="inch" enumval=2
+        Distance_Unit_inch,
+    }
+
+
+    export enum BME280_state {
+        //% block="temperature(℃)" enumval=0
+        BME280_temperature_C,
+
+        //% block="humidity(0~100)" enumval=1
+        BME280_humidity,
+
+        //% block="pressure(hPa)" enumval=2
+        BME280_pressure,
+
+        //% block="altitude(M)" enumval=3
+        BME280_altitude,
+    }
+
+
+
+
+
+
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BME280_I2C_ADDR, buf);
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+
+    function getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int8LE);
+    }
+
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt16LE);
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int16LE);
+    }
+    function get(): void {
+        let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
+        let t = var1 + var2
+        T = ((t * 5 + 128) >> 8) / 100
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
+        var2 = var2 + ((var1 * dig_P5) << 1)
+        var2 = (var2 >> 2) + (dig_P4 << 16)
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * dig_P1) >> 15
+        if (var1 == 0)
+            return; // avoid exception caused by division by zero
+        let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = (_p / var1) * 2;
+        var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * dig_P8) >> 13
+        P = _p + ((var1 + var2 + dig_P7) >> 4)
+        let adc_H = (getreg(0xFD) << 8) + getreg(0xFE)
+        var1 = t - 76800
+        var2 = (((adc_H << 14) - (dig_H4 << 20) - (dig_H5 * var1)) + 16384) >> 15
+        var1 = var2 * (((((((var1 * dig_H6) >> 10) * (((var1 * dig_H3) >> 11) + 32768)) >> 10) + 2097152) * dig_H2 + 8192) >> 14)
+        var2 = var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * dig_H1) >> 4)
+        if (var2 < 0) var2 = 0
+        if (var2 > 419430400) var2 = 419430400
+        H = (var2 >> 12) / 1024
+    }
+
+   
+
+    /**
+     * get pm2.5 value (μg/m³) 
+     * @param pm25pin describe parameter here, eg: DigitalPin.P14
+     */
+    //% advanced=true
+    //% blockId="readpm25" block="value of pm2.5(μg/m³) at pin %pm25pin"
+    export function ReadPM25(pm25pin: DigitalPin): number {
+        let pm25 = 0
+        while (pins.digitalReadPin(pm25pin) != 0) {
+        }
+        while (pins.digitalReadPin(pm25pin) != 1) {
+        }
+        pm25 = input.runningTimeMicros()
+        while (pins.digitalReadPin(pm25pin) != 0) {
+        }
+        pm25 = input.runningTimeMicros() - pm25
+        pm25 = pm25 / 1000 - 2
+        return pm25;
+    }
+
+
+
+    /**
+     * get pm10 value (μg/m³) 
+     * @param pm10pin describe parameter here, eg: DigitalPin.P13     
+     */
+    //% advanced=true
+    //% blockId="readpm10" block="value of pm10(μg/m³) at pin %pm10pin"
+    export function ReadPM10(pm10pin: DigitalPin): number {
+        let pm10 = 0
+        while (pins.digitalReadPin(pm10pin) != 0) {
+        }
+        while (pins.digitalReadPin(pm10pin) != 1) {
+        }
+        pm10 = input.runningTimeMicros()
+        while (pins.digitalReadPin(pm10pin) != 0) {
+        }
+        pm10 = input.runningTimeMicros() - pm10
+        pm10 = pm10 / 1000 - 2
+        return pm10;
+    }
+
+
+
+
+}
