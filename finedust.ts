3147699a8607680e94583ff6bb3f781009cd5576
ivyteen <neorafale@gmail.com>
2019-08-09 16:53:13 +0900

    finedust update

--- finedust.ts
+++ finedust.ts
@@ -190,21 +190,20 @@ namespace Finedust {
 
     /**
      * get pm2.5 value (μg/m³) 
-     * @param pm25pin describe parameter here, eg: DigitalPin.P14
+     * @param pm25pin describe parameter here, eg: DigitalPin.P15
      */
-    //% advanced=true
-    //% blockId="readpm25" block="value of pm2.5(μg/m³) at pin %pm25pin"
+    //% blockId="readpm25" block="%pm25pin|에서 pm2.5(μg/m³)값 읽기"
     export function ReadPM25(pm25pin: DigitalPin): number {
         let pm25 = 0
         while (pins.digitalReadPin(pm25pin) != 0) {
         }
         while (pins.digitalReadPin(pm25pin) != 1) {
         }
-        pm25 = input.runningTimeMicros()
+        pm25 = input.runningTime()
         while (pins.digitalReadPin(pm25pin) != 0) {
         }
-        pm25 = input.runningTimeMicros() - pm25
-        pm25 = pm25 / 1000 - 2
+        pm25 = input.runningTime() - pm25
+        pm25 = pm25 - 2
         return pm25;
     }
 
@@ -212,21 +211,20 @@ namespace Finedust {
 
     /**
      * get pm10 value (μg/m³) 
-     * @param pm10pin describe parameter here, eg: DigitalPin.P13     
+     * @param pm10pin describe parameter here, eg: DigitalPin.P16     
      */
-    //% advanced=true
-    //% blockId="readpm10" block="value of pm10(μg/m³) at pin %pm10pin"
+    //% blockId="readpm10" block="%pm10pin|에서 pm10(μg/m³)값 읽기"
     export function ReadPM10(pm10pin: DigitalPin): number {
         let pm10 = 0
         while (pins.digitalReadPin(pm10pin) != 0) {
         }
         while (pins.digitalReadPin(pm10pin) != 1) {
         }
-        pm10 = input.runningTimeMicros()
+        pm10 = input.runningTime()
         while (pins.digitalReadPin(pm10pin) != 0) {
         }
-        pm10 = input.runningTimeMicros() - pm10
-        pm10 = pm10 / 1000 - 2
+        pm10 = input.runningTime() - pm10
+        pm10 = pm10 - 2
         return pm10;
     }
 
